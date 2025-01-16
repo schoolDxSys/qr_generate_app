91450caa134c096702686a9042e16e8cd8ee9cd1
Sanchi7412 <yuusuke7412@gmail.com>
2025-01-16 20:12:13 +0900

    webアプリ, oauth対応

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 // 科目登録 {"Subject":"数学","Time":"2025-12-01 00:00:00.000","Status":"subject"}
 // 出席登録 {"userID":"{userid}","Serial":"{serial}","Time":"2025-12-01 00:00:00.000","Status":"attend"}
 
-function main(e) {
+function doPost(e) {
   let contents = e.postData.getDataAsString();
 
   let params = JSON.parse(contents);
   let status = params.Status;
+
+  console.log(status);
 
   if (status === "subject") {
     subject_controller(params);
@@ -14,4 +16,16 @@ function main(e) {
   } else {
     console.log("Error: Invalid status");
   }
+}
+
+function test() {
+  let contents = JSON.stringify({
+    postData: {
+      Subject: "数学",
+      Time: "2025-12-01 00:00:00.000",
+      Status: "subject",
+    },
+  });
+
+  main(contents);
 }
